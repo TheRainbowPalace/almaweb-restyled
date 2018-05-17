059d11b4d0061d1e464ac5e16aa9596384a8d66e
Jakob <jakobrdev@gmail.com>
2018-05-17 20:58:45 +0200

    Create version 2018.1.0

---
+++ src/inject.js
@@ -0,0 +1,26 @@
+// The style variable gets injected at build time
+style = `!StylePlaceholder`
+
+/* Inject a hamburger button (which toggles the
+*  sidebar) into the website:
+*/
+head = document.getElementsByTagName('head')[0]
+head.innerHTML += '<style>' + style + '</style>'
+
+sidebar = document.getElementById('pageHead')
+
+menuButton = document.createElement('button')
+menuButton.innerText = ""
+menuButton.id = 'showSidebar'
+menuButton.addEventListener('click', () => sidebar.classList.toggle('open'))
+
+document.body.addEventListener('click', (e) =>
+{
+  if (e.target !== menuButton && !sidebar.contains(e.target))
+  {
+    sidebar.classList.remove('open')
+  }
+})
+
+topBar = document.getElementById('acc_pageDescription')
+topBar.appendChild(menuButton)
